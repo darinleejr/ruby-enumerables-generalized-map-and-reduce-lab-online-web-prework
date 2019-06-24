c36b684dc2470366c759e7bf0634a3ac00e22e65
Darin Webster <darinleenavy@gmail.com>
2019-06-24 21:16:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,11 +4,21 @@ def map(array)
   map = array.map { |n| n * -1}
 end
 
-def map_org(dune)
+def map(dune)
   dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
   
   map = dune.map { |n| n}
 end
 
+def map(array)
+  array = [1, 2, 3, -9]
+  
+  map = array.map { |n| n * 2}
+end
 
+def map(array)
+  array [1, 2, 3, -9]
+  
+  map = array.map { |n| n * n }
+end
 
