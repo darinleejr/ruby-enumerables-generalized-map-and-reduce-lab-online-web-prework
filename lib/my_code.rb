0051fe2d678804686511fe883f6b374b37efd4e7
Darin Webster <darinleenavy@gmail.com>
2019-06-24 21:11:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,15 @@ def map(dune)
   map = dune.map { |n| n}
 end
 
+def map(array)
+  array = [1, 2, 3, -9]
+  
+  map = array.map { |n| n * 2}
+end
+
+def map(array)
+  array [1, 2, 3, -9]
+  
+  map = array.map { |n| n **}
+end
+
